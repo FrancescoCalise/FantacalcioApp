d9dcc0b44175a7b40f6c615819a0891a6ff458a9
Francesco <francescocalise1994@gmail.com>
2019-08-21 12:11:56 +0200

    modify environment

--- Client/src/environments/environment.prod.ts
+++ Client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //api: "http://10.1.1.8:1000/api/"
-  api:"http://192.168.1.9:1000/api/"
+  api: 'http://10.1.1.8:1000/api/'
+  // api: "http://192.168.1.9:1000/api/"
 };
